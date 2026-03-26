6e22a1edb318e41cd19341100f0ebd4abe65c32c
alexandre.wiemann@gmail.com <alexandre.wiemann@gmail.com>
2026-03-26 16:18:34 +0100

    test(db) : schema and seed tested

--- database/seed.sql
+++ database/seed.sql
@@ -3,11 +3,11 @@
 -- Exécuter après schema.sql : psql -U minilib_user -d minilib -f database/seed.sql
 
 INSERT INTO livres (isbn, titre, auteur, annee, genre) VALUES
-('9780132350884', 'Clean Code', 'Robert C. Martin', '2008'),
-('9780201633610', 'Design Patterns', 'Gang of Four', '1994'),
-('9782070612758', 'Le Petit Prince', 'Saint-Exupéry', '1943'),
-('9782070360024', '1984', 'George Orwell', '1949'),
-('9780596517748', 'JavaScript: Good Parts', 'D. Crockford', '2008');
+('9780132350884', 'Clean Code', 'Robert C. Martin', '2008', 'Informatique'),
+('9780201633610', 'Design Patterns', 'Gang of Four', '1994', 'Informatique'),
+('9782070612758', 'Le Petit Prince', 'Saint-Exupéry', '1943', 'Roman'),
+('9782070360024', '1984', 'George Orwell', '1949', 'Roman'),
+('9780596517748', 'JavaScript: Good Parts', 'D. Crockford', '2008', 'Informatique');
 
 INSERT INTO adherents (numero_adherent, nom, prenom, email) VALUES
 ('ADH-001', 'Martin', 'Alice', 'alice.martin@email.com'),
@@ -19,4 +19,4 @@ INSERT INTO emprunts (livre_id, adherent_id, date_retour_prevue) VALUES
 (3, 1, CURRENT_DATE + INTERVAL '14 days');
 
 -- Marquer un livre comme indisponible
-UPDATE livres SET disponible = false WHERE id = 3;
+UPDATE livres SET disponible = false WHERE id = 3;\
