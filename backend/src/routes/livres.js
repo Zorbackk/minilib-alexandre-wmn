f8adaa343599a9b2bb8152c70095ecc1ea34a6a8
alexandre.wiemann@gmail.com <alexandre.wiemann@gmail.com>
2026-03-28 19:55:38 +0100

    refactor(router/livres) : implementing asyncWrapper

--- backend/src/routes/livres.js
+++ backend/src/routes/livres.js
@@ -3,31 +3,33 @@
 // Toutes ces routes sont préfixées par /api/v1/livres dans app.js
 
 import express from 'express';
-const router = express.Router();
+import asyncWrapper from '../middleware/asyncWrapper.js'
 import * as controller from '../controllers/livresControllers.js'
 import validateLivre from '../middleware/validateLivre.js';
 
+const router = express.Router();
+
 // GET /api/v1/livres → liste tous les livres (+ filtres query params)
-router.get('/', controller.getLivres);
+router.get('/', asyncWrapper(controller.getLivres));
 
 // GET  /api/v1/livres/recherche?q=clean
 // Simplification de la route en passant la méthode dans le controller
-router.get('/recherche', controller.rechercherLivres)
+router.get('/recherche', asyncWrapper(controller.rechercherLivres))
 // Cette route doit être AVANT router.get(':id') 
 // Sinon Express interpréterait 'recherche' comme un id !
 
 // GET /api/v1/livres/:id → détail d'un livre
-router.get('/:id', controller.getLivreById);
+router.get('/:id', asyncWrapper(controller.getLivreById));
 
 // POST /api/v1/livres → créer un nouveau livre
 // Middlewar validateLivre va vérifier la présence de l'ensemble des champs avant
 // de passer au controller
-router.post('/', validateLivre, controller.createLivre);
+router.post('/', validateLivre, asyncWrapper(controller.createLivre));
 
 // PUT /api/v1/livres/:id → modifier un livre
-router.put('/:id', validateLivre, controller.updateLivre);
+router.put('/:id', validateLivre, asyncWrapper(controller.updateLivre));
 
 // DELETE /api/v1/livres/:id → supprime un livre
-router.delete('/:id', controller.deleteLivre);
+router.delete('/:id', asyncWrapper(controller.deleteLivre));
 
 export default router;
