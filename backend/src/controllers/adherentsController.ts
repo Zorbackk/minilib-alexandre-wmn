3684bf511f1c6fa370c15194cce8eb461b7c69aa
alexandre.wiemann@gmail.com <alexandre.wiemann@gmail.com>
2026-04-01 10:31:13 +0200

    refactor(typescript) : adherentsController  migrated

--- backend/src/controllers/adherentsController.ts
+++ backend/src/controllers/adherentsController.ts
@@ -4,14 +4,15 @@ import * as adherentsModel from '../models/adherentsModel.js'
 import { RequestHandler } from 'express';
 
 /** GET /api/v1/adherents */
-export const getAdherents = async (req, res) => {
+export const getAdherents : RequestHandler = async (req, res) => {
   const adherents = await adherentsModel.findAll();
   res.json(adherents);
 }
 
 /** GET /api/v1/adherents/:id */
-export const getAdherentById = async (req, res) => {
-  const adherent = await adherentsModel.findById(req.params.id);
+export const getAdherentById : RequestHandler = async (req, res) => {
+  const id = parseInt(req.params.id as string);
+  const adherent = await adherentsModel.findById(id);
   if (!adherent) 
     return res.status(404).json({ erreur : `Adhérent id:${req.params.id} non trouvé`});
   res.json(adherent);
@@ -19,7 +20,7 @@ export const getAdherentById = async (req, res) => {
 
 
 /** POST /api/v1/adherents */
-export const createAdherent = async (req, res) => {
+export const createAdherent : RequestHandler = async (req, res) => {
   const {nom, prenom, email} = req.body;
   const manquants = ['nom', 'prenom', 'email'].filter(k => !req.body[k]);
   if (manquants.length > 0) 
@@ -29,8 +30,9 @@ export const createAdherent = async (req, res) => {
 };
 
 /** DELETE /api/v1/adherents/:id - soft delete */
-export const desactiverAdherent = async (req, res) => {
-  const adherent = await adherentsModel.desactiver(req.params.id);
+export const desactiverAdherent : RequestHandler = async (req, res) => {
+  const id = parseInt(req.params.id as string)
+  const adherent = await adherentsModel.desactiver(id);
   if (!adherent)
     return res.status(404).json({erreur: `Adhérent id:${req.params.id} non trouvé`});
   res.json(adherent);
